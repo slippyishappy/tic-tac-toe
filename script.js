85320e37219bf81f86e8fbdfbf84bde7ec56425f
Slippy <116444403+slippyishappy@users.noreply.github.com>
2024-07-29 15:28:44 +0100

    Trying to figure out how to play the game in the console (still didn't finish)

---
+++ script.js
@@ -0,0 +1,41 @@
+function Player(marker) {
+    let score = 0;
+    const addScore = () => score++;
+    const getScore = () => score;
+
+    return {marker, getScore, addScore};
+};
+
+function startGame() {
+    
+    let gameBoard = [];
+    const rows = 3;
+    const columns = 3;
+    
+    for (let i = 0; i < rows; i++) {
+        gameBoard[i] = [];
+        for (let j = 0; j < columns; j++) {
+            gameBoard[i][j] = [];
+        }
+    }
+
+    let putMarker = (player) => {
+        let chosenRow = prompt(`What row would you like to place ${player.marker}?`);
+        let chosenColumn = prompt(`What about the column? ${player.marker}`);
+
+        if (gameBoard[parseInt(chosenRow) - 1][parseInt(chosenColumn) - 1] === 0) {
+            gameBoard[parseInt(chosenRow) - 1][parseInt(chosenColumn) - 1] = player.marker;
+            console.log(gameBoard);
+        } else {
+            alert('Place is cell, please choose another cell');
+            putMarker(player);
+        }
+    }
+    
+    return {playerX, playerO, gameBoard, putMarker};
+};
+
+const playerX = Player('X');
+const playerO = Player('O');
+
+let game1 = startGame();
