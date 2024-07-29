1d5e6d1eb6ed1fc586d7c8b5ebeca24232ce4d8c
Slippy <116444403+slippyishappy@users.noreply.github.com>
2024-07-29 20:10:23 +0100

    Made the game in the console

--- script.js
+++ script.js
@@ -6,8 +6,7 @@ function Player(marker) {
     return {marker, getScore, addScore};
 };
 
-function startGame() {
-    
+function setGame() {
     let gameBoard = [];
     const rows = 3;
     const columns = 3;
@@ -27,15 +26,78 @@ function startGame() {
             gameBoard[parseInt(chosenRow) - 1][parseInt(chosenColumn) - 1] = player.marker;
             console.log(gameBoard);
         } else {
-            alert('Place is cell, please choose another cell');
+            alert('Cell is full, please choose another cell');
             putMarker(player);
         }
     }
-    
-    return {playerX, playerO, gameBoard, putMarker};
+    return {gameBoard, putMarker};
 };
 
-const playerX = Player('X');
-const playerO = Player('O');
+function gameController() {
+    const playerX = Player('X');
+    const playerO = Player('O');
 
-let game1 = startGame();
+    let players = [playerX, playerO];
+
+    let currentPlayer = players[0];
+
+    let game = setGame();
+
+    const switchTurns = () => {
+        currentPlayer = currentPlayer === players[0] ? players[1] : players[0];
+    };
+
+    const playGame = () => {
+        let result;
+        do {
+            console.log(`Player ${currentPlayer.marker}'s turn`);
+            game.putMarker(currentPlayer);
+            result = checkWinner();
+            if (result) {
+                if (result === 'tie') {
+                    console.log("It's a tie!");
+                } else {
+                    console.log(`Player ${result} wins!`);
+                }
+            } else {
+                switchTurns();
+            }
+        } while (!result);
+    };
+
+    const checkWinner = () => {
+        for (let i = 0; i < 3; i++) {
+            if (game.gameBoard[i][0] && game.gameBoard[i][0] ===
+                game.gameBoard[i][1] && game.gameBoard[i][0] === 
+                game.gameBoard[i][2]) {
+                return game.gameBoard[i][0];
+            }
+        }
+    
+        // Check columns
+        for (let j = 0; j < 3; j++) {
+            if (game.gameBoard[0][j] && 
+                game.gameBoard[0][j] === game.gameBoard[1][j] && 
+                game.gameBoard[0][j] === game.gameBoard[2][j]) {
+                return game.gameBoard[0][j];
+            }
+        }
+
+            // Check diagonals
+        if (game.gameBoard[0][0] && 
+            game.gameBoard[0][0] === game.gameBoard[1][1] && 
+            game.gameBoard[0][0] === game.gameBoard[2][2]) {
+            return game.gameBoard[0][0];
+            }
+
+        if (game.gameBoard[0][2] && 
+            game.gameBoard[0][2] === game.gameBoard[1][1] && 
+            game.gameBoard[0][2] === game.gameBoard[2][0]) {
+            return game.gameBoard[0][2];
+        }
+        }
+
+    playGame();
+};
+
+gameController();
